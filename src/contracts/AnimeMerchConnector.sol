5994cfed441ac7e167738e4faad3273c867d85c1
AcmeGamers <52010365+AcmeGamers@users.noreply.github.com>
2022-06-06 07:38:04 +0500

    Adding contracts

---
+++ src/contracts/AnimeMerchConnector.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// import './ERC721Metadata.sol';
+// import './ERC721Enumerable.sol';
+import './ERC721Metadata.sol';
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol';
+
+contract ERC721Connector is ERC721Metadata, ERC721Enumerable {
+
+    // we deploy connector right away
+    // we want to carry the metadata info over
+
+    constructor(string memory name, string memory symbol)  ERC721Metadata(name, symbol) {
+
+    }
+
+}
